fdfaf6d0f08d19a202868a278fc1dddbb6a2020a
Fredrik Emil Bakke <fbak@netlight.com>
2020-08-31 20:39:17 +0200

    remove blue background

--- src/components/ToDoCanvas.tsx
+++ src/components/ToDoCanvas.tsx
@@ -6,61 +6,61 @@ import { CSSProperties } from '@material-ui/core/styles/withStyles';
 
 
 const ToDoItem = (props: any) => {
- const { id, x, y, text, updateItemPos, handleCardPress } = props
+  const { id, x, y, text, updateItemPos, handleCardPress } = props
 
 
- const handleDrag = (event: any, ui: any) => {
+  const handleDrag = (event: any, ui: any) => {
 
-  updateItemPos(id)(parseInt(x) + ui.deltaX, parseInt(y) + ui.deltaY)
- }
+    updateItemPos(id)(parseInt(x) + ui.deltaX, parseInt(y) + ui.deltaY)
+  }
 
- const cardStyles: CSSProperties = {
-  backgroundColor: '#efefef',
-  width: 100,
-  height: 100,
-  position: 'absolute',
-  zIndex: 9001,
-  cursor: 'pointer'
- }
+  const cardStyles: CSSProperties = {
+    backgroundColor: '#efefef',
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    zIndex: 9001,
+    cursor: 'pointer'
+  }
 
- return (
-  <Draggable
-   axis="both"
-   handle=".handle"
-   position={{ x, y }}
-   onDrag={handleDrag}
-  >
-   <Box p={2} onClick={handleCardPress(id)} className="handle" style={cardStyles}>
-    {text}
-   </Box>
+  return (
+    <Draggable
+      axis="both"
+      handle=".handle"
+      position={{ x, y }}
+      onDrag={handleDrag}
+    >
+      <Box p={2} onClick={handleCardPress(id)} className="handle" style={cardStyles}>
+        {text}
+      </Box>
 
-  </Draggable>
- )
+    </Draggable>
+  )
 }
 
 export default (props: any) => {
- const { toDoItems, setToDoItems, setSelectedToDo } = props
+  const { toDoItems, setToDoItems, setSelectedToDo } = props
 
- if (!toDoItems) {
-  return null
- }
+  if (!toDoItems) {
+    return null
+  }
 
- const handleCardPress = (itemId: number) => () => {
-  setSelectedToDo(itemId)
- }
+  const handleCardPress = (itemId: number) => () => {
+    setSelectedToDo(itemId)
+  }
 
- const updateItemPos = (itemId: number) => (x: number, y: number) => {
-  setToDoItems({
-   ...toDoItems, [itemId]: {
-    ...toDoItems[itemId],
-    x,
-    y
-   }
-  })
- }
+  const updateItemPos = (itemId: number) => (x: number, y: number) => {
+    setToDoItems({
+      ...toDoItems, [itemId]: {
+        ...toDoItems[itemId],
+        x,
+        y
+      }
+    })
+  }
 
- return <Box style={{ backgroundColor: 'blue', height: '100%' }}>
-  {Object.values(toDoItems).map(toDo => <ToDoItem {...toDo} updateItemPos={updateItemPos} handleCardPress={handleCardPress} />)}
- </Box>
+  return <Box style={{ height: '100%' }}>
+    {Object.values(toDoItems).map(toDo => <ToDoItem {...toDo} updateItemPos={updateItemPos} handleCardPress={handleCardPress} />)}
+  </Box>
 
 }
